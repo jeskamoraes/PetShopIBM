2943ce6d5a76827352114e7dd99eca2f31b218c2
Jessica Santos <jm.jeska@gmail.com>
2022-08-26 15:47:52 -0300

    Implementando dtos para entidades Animal e Cliente

---
+++ src/main/java/br/com/ibm/petshop/dtos/AnimalDto.java
@@ -0,0 +1,10 @@
+package br.com.ibm.petshop.dtos;
+
+import br.com.ibm.petshop.models.Cliente;
+
+public class AnimalDto {
+	Cliente cliente;
+	String nome;
+	int idade;
+	float peso;
+}
---
+++ src/main/java/br/com/ibm/petshop/dtos/ClienteDto.java
@@ -0,0 +1,7 @@
+package br.com.ibm.petshop.dtos;
+
+public class ClienteDto {
+	
+	String nome;
+	String telefone;
+}
